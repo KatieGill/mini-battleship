4eed86307b99a6f1c6620aa12ac2a465add5336b
Katie Gill <katiegill@Katies-Laptop.local>
2024-05-30 15:23:42 -0400

    part-1 update. guess no longer case sensitive

--- part-1.js
+++ part-1.js
@@ -1,6 +1,6 @@
-const rs = require('readline-sync');
+const rs = require("readline-sync");
 
-const grid = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
+const grid = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"];
 let gridPlacementTracker = [];
 let guessTracker = [];
 let shipsRemaining = 2;
@@ -14,43 +14,43 @@ const setGrid = (grid) => {
     gridPlacementTracker.push(gridPlacement);
     return gridPlacement;
   }
-}
+};
 
 const resetGrid = (grid) => {
   gridPlacementTracker = [];
   guessTracker = [];
   shipsRemaining = 2;
   return battleship(grid);
-}
+};
 
 const playerGuess = (grid) => {
   return rs.question(`Enter a location to strike, i.e. 'A2': `, {
     limit: grid,
     limitMessage: `That is not a valid location`,
-    caseSensitive: true
   });
-}
+};
 
 const guessResult = (guess) => {
   if (guessTracker.includes(guess)) {
     console.log(`You have already picked this location. Miss!`);
   } else if (fleet.includes(guess)) {
     shipsRemaining -= 1;
-    const message = shipsRemaining === 1 
-      ? `Hit! You have sunk a battleship. 1 ship remaining.`
-      : `You have destroyed all battleships!`;
-      console.log(message); 
-    } else {
+    const message =
+      shipsRemaining === 1
+        ? `Hit! You have sunk a battleship. 1 ship remaining.`
+        : `You have destroyed all battleships!`;
+    console.log(message);
+  } else {
     console.log(`You have missed!`);
   }
   if (!guessTracker.includes(guess)) {
     guessTracker.push(guess);
   }
   return shipsRemaining;
-}
+};
 
 const gamePlay = (grid) => {
-  let guess = playerGuess(grid);
+  let guess = playerGuess(grid).toUpperCase();
   guessResult(guess);
   if (shipsRemaining > 0) {
     return gamePlay(grid);
@@ -59,12 +59,12 @@ const gamePlay = (grid) => {
       return resetGrid(grid);
     }
   }
-}
+};
 
 const battleship = (grid) => {
   rs.keyIn(`Press any key to start the game!`);
   fleet = [1, 2].map(() => setGrid(grid));
   return gamePlay(grid);
-}
+};
 
 battleship(grid);
